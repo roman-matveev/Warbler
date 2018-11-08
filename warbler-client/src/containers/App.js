42bdcd878f045c4fd3a5c4843c82951c59b84afa
Roman Matveev <roman.x@live.com>
2018-11-08 14:56:51 -0500

    Added Navbar container with styling. Reverted Redux to 3.7.2.

--- warbler-client/src/containers/App.js
+++ warbler-client/src/containers/App.js
@@ -1,15 +1,16 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {BrowserRouter as Router} from 'react-router-dom';
 import {Provider} from 'react-redux';
 import {configureStore} from '../store';
+import Navbar from './Navbar';
 
-const store = configureStore;
+const store = configureStore();
 
 const App = () => (
     <Provider store={store}>
         <Router>
-            <div>
-                Hello World
+            <div className='onboarding'>
+                <Navbar />
             </div>
         </Router>
     </Provider>
